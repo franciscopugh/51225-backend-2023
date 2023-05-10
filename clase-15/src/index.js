29d85817547352eb8c2f74c0bb519524488b9e64
Francisco Pugh <franciscopugh01@gmail.com>
2023-05-10 12:03:42 -0300

    Clase 15

--- clase-15/src/index.js
+++ clase-15/src/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 import { userModel } from './models/user.js'
 const app = express()
 
-mongoose.connect("mongodb+srv://franciscopugh01:coderhouse@cluster0.uwq84bc.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://franciscopugh01:<password>@cluster0.uwq84bc.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log("DB is connected"))
     .catch((error) => console.log("Errror en MongoDB Atlas :", error))
 
